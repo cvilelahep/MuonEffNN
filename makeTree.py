6c4efbe31197be8e79c632f1a5ede903d1cfc3e3
Zheyang Chen <zchen@ivy.physics.sunysb.edu>
2020-12-18 10:30:41 -0500

    test

---
+++ makeTree.py
@@ -0,0 +1,120 @@
+import torch
+import muonEffModel
+
+import os
+import glob
+
+import pandas as pd
+import numpy as np
+import uproot
+
+import math
+
+CAF_FHC_fName = "/storage/shared/zchen/newcaf/00/FHC.*.CAF.root"
+#CAF_RHC_fName = "/storage/shared/cvilela/CAF/mcc11_v4/ND_RHC_FV_*.root"
+
+CAF_files = glob.glob(CAF_FHC_fName)
+#CAF_files += glob.glob(CAF_RHC_fName)
+
+# Process bachSize events at a time
+batchSize = 1000
+
+outDir = "/home/zchen/neural/newcaf/newindex/muonfile/epoch50new"
+
+try :
+    os.makedirs(outDir+"/"+"outTrees")
+except(FileExistsError) :
+    pass
+
+# Get saved model
+net = muonEffModel.muonEffModel().cuda()
+net.load_state_dict(torch.load(outDir+"/"+"muonEff.nn"))
+net.eval()
+
+for f in CAF_files :
+    
+    # Read in file
+    varList = ['LepMomX',
+               'LepMomY', 
+               'LepMomZ',
+               'vtx_x',
+               'vtx_y',
+               'vtx_z',
+               'muon_contained',
+               'muon_tracker', 
+               'isCC',
+               'LepPDG',
+               'Ev',
+               'muon_endpoint']
+    try :
+        print(f)
+        fUprootIn = uproot.open(f)
+        tIn = fUprootIn['caf']
+    except :
+        print("Didn't find caf tree. Skipping...")
+        continue
+    
+    dIn = tIn.arrays(varList, outputtype = pd.DataFrame)
+    
+    features = dIn[['LepMomX', 'LepMomY', 'LepMomZ', 'vtx_x', 'vtx_y', 'vtx_z']]
+    
+    N_Events = len(dIn)
+    N_Batches = math.ceil(float(N_Events)/batchSize)
+
+    predictions = []
+
+    for i in range(0, N_Batches) :
+        print("Processing batch "+str(i)+" out of "+str(N_Batches))
+        N_First = i*batchSize
+        if (i+1)*batchSize <= N_Events :
+            N_Last = (i+1)*batchSize
+        else :
+            N_Last = N_Events
+        
+        data = torch.as_tensor(features.iloc[N_First:N_Last].values).type(torch.FloatTensor).cuda()
+        
+        prediction = net(data)
+        
+        prediction = torch.nn.functional.softmax(prediction)
+        
+        predictions.append(prediction.cpu().detach().numpy())
+        
+        del data
+        
+    predictions = np.concatenate(predictions)
+    print(predictions.shape)
+    outFname = outDir+"/"+"outTrees"+"/"+os.path.splitext(os.path.basename(f))[0]+"_MuonEff.root"
+    
+    with uproot.recreate(outFname) as f:
+        f["muonEfficiency"] = uproot.newtree({'LepMomX' : np.float64,
+                                              'LepMomY' : np.float64,
+                                              'LepMomZ' : np.float64,
+                                              'vtx_x' : np.float64,
+                                              'vtx_y' : np.float64,
+                                              'vtx_z' : np.float64,
+                                              'muon_endpoint_x' : np.float64,
+                                              'muon_endpoint_y' : np.float64,
+                                              'muon_endpoint_z' : np.float64,
+                                              'muon_contained' : np.int32,
+                                              'muon_tracker' : np.int32,
+                                              'isCC' : np.int32,
+                                              'LepPDG' : np.int32,
+                                              'Ev' : np.float64,
+                                              'nnEffContained' : np.float64,
+                                              'nnEffTracker' : np.float64,
+                                              'nnEffNotAcc'  : np.float64})
+
+
+        extend_dict = {}
+        for var in varList :
+          if var == 'muon_endpoint':
+            pass
+          else:
+            extend_dict[var] = dIn[var].values
+        extend_dict['nnEffContained'] = predictions[:,0]
+        extend_dict['nnEffTracker'] = predictions[:,1]
+        extend_dict['nnEffNotAcc'] = predictions[:,2]
+        extend_dict['muon_endpoint_x'] = dIn['muon_endpoint[0]']
+        extend_dict['muon_endpoint_y'] = dIn['muon_endpoint[1]']
+        extend_dict['muon_endpoint_z'] = dIn['muon_endpoint[2]'] 
+        f["muonEfficiency"].extend(extend_dict)
---
+++ muonEffDataset.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+
+from torch.utils import data
+
+class muonEffDataset(data.Dataset):
+      def __init__(self, archive, datasetName):
+            self.store = pd.HDFStore(archive, 'r')
+            self.df = pd.read_hdf(self.store, datasetName)
+
+      def __getitem__(self, index):
+            return self.df.iloc[index, [self.df.columns.get_loc(col) for col in ['LepMomX', 'LepMomY', 'LepMomZ', 'vtx_x', 'vtx_y', 'vtx_z']]], self.df.iloc[index, [self.df.columns.get_loc(col) for col in ['new_contained', 'new_tracker']]]
+      
+      def __len__(self):
+            return len(self.df)
+
+      def close(self):
+            del self.df
+            self.store.close()
+
+def collate(batch) :
+      result = []
+      for i in range(len(batch[0])):
+            result.append(np.array([sample[i] for sample in batch]))
+      return tuple(result)
